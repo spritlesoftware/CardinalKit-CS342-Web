e816f138dd225df1cfa1cb7729c266117a4df208
varunkumar30999 <varunkumar0930@gmail.com>
2021-02-03 15:42:50 +0400

    Changes in UI

--- ReactJS-Project-CardinalKit/src/components/SideBar.jsx
+++ ReactJS-Project-CardinalKit/src/components/SideBar.jsx
@@ -84,7 +84,7 @@ const SideBar = () => (
               >
                 <path d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
               </svg>
-              <span className="ml-4">Setting</span>
+              <span className="ml-4">Settings</span>
             </Link>
           </li>
         </ul>
@@ -110,7 +110,7 @@ const SideBar = () => (
               >
                 <path d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z" />
               </svg>
-              <span className="ml-4">Create Survey</span>
+              <span className="ml-4">Create Surveys</span>
             </Link>
           </li>
         </ul>
