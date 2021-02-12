a52f121cfff44ccd1a29892c34007ef7a518f326
varun kumar <varuns@spritle.com>
2021-02-12 10:44:47 +0530

    download feature response in progress\

--- ReactJS-Project-CardinalKit/src/components/UserList.tsx
+++ ReactJS-Project-CardinalKit/src/components/UserList.tsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React, { Component } from 'react';
+import React, { Component } from 'react';   
 import { Link } from 'react-router-dom';
 import ReactTable from 'react-table-6';
 import 'react-table-6/react-table.css';
@@ -66,13 +66,23 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
     }
   }
 
+  downloadCsv = (uid) => {
+    // console.log(uid)
+    getSurveys(uid)
+      .then((querySnapshot) => {
+        return querySnapshot.docs.map((doc) => {
+          console.log(doc.data()?.surveyQuestionId)
+        })
+      })
+  }
+
   getSurveyDetails = () => {
     const { users } = this.state;
     const surveyData: any[] = [];
-
     users.map(({ userId }) => {
 
       return getSurveys(userId).then(querySnapshot => {
+
         surveyData.push(querySnapshot.docs[0].data());
         const data = surveyData.map(doc => {
 
@@ -89,13 +99,14 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
             view: (
               <div>
                 <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                  <Link to={'/users/' + doc.userId} className="animate-bounce ease-out duration-700">View Surveys</Link>
+                  <Link to={`/users/${doc.userId}`} className="animate-bounce ease-out duration-700">View Surveys</Link>
                 </span>
-                <a href="#">
-                  <span className="px-2 py-1  font-semibold leading-tight rounded-full dark:bg-blue-700 dark:text-blue-100">
-                    <i className="fas fa-download animate-bounce ease-out hover:scale-50" />
+                <button onClick={() => this.downloadCsv(doc.userId)}>
+                  <span className="px-2 py-1  font-semibold bg-blue-200 leading-tight rounded-full dark:bg-blue-700 dark:text-blue-100">
+                    Response
+                    <i className="ml-1 fas fa-cloud-download-alt  text-gray-700	animate-bounce ease-out hover:scale-50" />
                   </span>
-                </a>
+                </button>
               </div>
             ),
           };
@@ -112,7 +123,7 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
     const columns = [
       {
         Header: () => (
-          <div className="text-xs text-center font-semibold tracking-wide text-left text-gray-500 uppercase dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+          <div className="text-xs text-center shadow-none font-semibold tracking-wide text-gray-500 uppercase dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
             Name
           </div>
         ),
@@ -131,7 +142,7 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
         accessor: 'email',
         className: 'font',
         width: 250,
-        Cell: row => <div className="text-center h-4">{row.value}</div>
+        Cell: row => <div className="text-center h-6">{row.value}</div>
       },
       {
         Header: () => (
@@ -160,7 +171,7 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
     return (
       <div className="container px-6 mx-auto grid">
         <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
-          <div className="flex items-center p-4 bg-white rounded-lg shadow-lg dark:bg-gray-800">
+          <div className="flex items-center p-4 bg-white rounded-lg shadow-lg  transition duration-1000 ease-in dark:bg-gray-800">
             <div className="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"></path>
@@ -219,6 +230,7 @@ class UserList extends Component<{}, { users: any[]; newUsers: any[]; totalSurve
           defaultPageSize={5}
           PaginationComponent={Pagination}
         />
+        {/* {console.log(this.state.users)} */}
 
         <h2 className="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Charts</h2>
         <div className="grid gap-6 mb-8 md:grid-cols-2">
