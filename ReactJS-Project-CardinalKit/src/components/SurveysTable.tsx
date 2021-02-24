1d189e8269996739c5b8c107badfcbf0afd6ef81
varunkumar3091999 <varunkumar0930@gmail.com>
2021-02-24 21:43:41 +0400

    render actual choice answers instead of their index

--- ReactJS-Project-CardinalKit/src/components/SurveysTable.tsx
+++ ReactJS-Project-CardinalKit/src/components/SurveysTable.tsx
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import * as React from 'react';
 import { connect } from 'react-redux';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Dispatch } from 'redux';
 import Pagination from './Pagination'
 
@@ -57,7 +57,7 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
     this.state = {
       surveyIds: [],
       surveyList: [],
-      
+
     }
   }
 
@@ -77,14 +77,14 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
           .then((data) => {
             if (data.payload) {
               const startDate = moment(data?.payload?.startDate.substring(0, 10)).format('ll')
-              const identifier = data?.payload?.name || '(Survey Name unavailable)'
+              const identifier = data?.payload?.name || data?.payload?.identifier
               const surveyData = {
                 startDate,
                 identifier,
                 view:
                   <div>
                     <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
-                    <Link to={`${window.location.pathname}/${surveyId}`}>View Response</Link>
+                      <Link to={`${window.location.pathname}/${surveyId}`}>View Response</Link>
                     </span>
                   </div>
               }
@@ -103,7 +103,7 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
   //                           .then((question) => {
   //                             return question.name
   //                           })
-    
+
   //   console.log(questionName)
   //   return questionName
   // }
@@ -136,7 +136,7 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
         accessor: 'startDate',
         className: "px-4 py-3 text-sm",
         width: 300,
-    Cell: row => <div style={{ textAlign: "center" }}>{row.value}</div>
+        Cell: row => <div style={{ textAlign: "center" }}>{row.value}</div>
       },
       {
         Header: () => (
@@ -157,7 +157,7 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
             defaultPageSize={5}
             PaginationComponent={Pagination}
             className={"ReactTable " + (this.state.surveyList === [] ? 'animate-pulse' : '')}
-            // filterable={true}
+          // filterable={true}
           />
         </div>
       </div>
