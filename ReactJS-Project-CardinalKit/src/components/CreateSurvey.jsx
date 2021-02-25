e1ff9a91e5d874c048fae28f154e7b0afe941f09
varun kumar <varuns@spritle.com>
2021-02-25 18:17:32 +0530

    bug fix, bug-can create survey without selecting the question type, fixed

--- ReactJS-Project-CardinalKit/src/components/CreateSurvey.jsx
+++ ReactJS-Project-CardinalKit/src/components/CreateSurvey.jsx
@@ -124,8 +124,9 @@ const CreateSurvey = ({ history }) => {
             setQuestionAttributes(e, questionIndex);
           }}
           name="questionType"
+          required
         >
-          <option className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">
+          <option className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" value="">
             Please select...
           </option>
           <option
