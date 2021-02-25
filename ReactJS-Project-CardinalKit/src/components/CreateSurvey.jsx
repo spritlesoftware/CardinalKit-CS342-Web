e6de1b25c4f8f3b70d97520d0ebf533c8d8929a8
Siva <siva.kb@spritle.com>
2021-02-25 21:06:52 +0530

    added style

--- ReactJS-Project-CardinalKit/src/components/CreateSurvey.jsx
+++ ReactJS-Project-CardinalKit/src/components/CreateSurvey.jsx
@@ -125,6 +125,7 @@ const CreateSurvey = ({ history }) => {
           }}
           name="questionType"
           required
+          style={{width: "160px"}}
         >
           <option className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" value="">
             Please select...
