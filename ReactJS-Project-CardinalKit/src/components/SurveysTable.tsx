b4a46085d703072b0cca9d767cc76a7b4823777e
varun kumar <varuns@spritle.com>
2021-02-26 18:34:08 +0530

    button style change

--- ReactJS-Project-CardinalKit/src/components/SurveysTable.tsx
+++ ReactJS-Project-CardinalKit/src/components/SurveysTable.tsx
@@ -83,7 +83,7 @@ class SurveysTable extends React.Component<SurveyHeaderProps, State> {
                 identifier,
                 view:
                   <div>
-                    <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100">
+                    <span className="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 shadow-lg dark:bg-green-700 dark:text-green-100">
                       <Link to={`${window.location.pathname}/${surveyId}`}>View Response</Link>
                     </span>
                   </div>
