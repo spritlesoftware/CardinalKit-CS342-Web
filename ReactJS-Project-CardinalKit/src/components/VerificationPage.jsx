e51d9b54f70a4da6e10a88ce098687971f37cef3
varun kumar <varuns@spritle.com>
2021-02-26 18:29:16 +0530

    code optimisation

--- ReactJS-Project-CardinalKit/src/components/VerificationPage.jsx
+++ ReactJS-Project-CardinalKit/src/components/VerificationPage.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
+import { toast } from 'react-toastify';
 import logo from '../images/catdinalKitFemale.png';
 import logo2 from '../images/cardinal_logo.svg';
-import { toast } from 'react-toastify';
 
 class VerificationPage extends React.Component {
   constructor(props) {
@@ -11,35 +11,31 @@ class VerificationPage extends React.Component {
     };
   }
 
-  onSubmitHandler = (e) => {
-    e.preventDefault()
-    this.setState({
-      userCode: document.getElementById('verificationCode'),
-    });
+  onSubmitHandler = e => {
+    e.preventDefault();
     if (this.state.userCode === localStorage.getItem('verifyCode')) {
       window.sessionStorage.setItem('isLoggedIn', true);
       window.localStorage.clear();
-      this.props.history.push('/users')
-      window.location.reload()
-    }  else {
-      toast.warning('Incorrect verfication code!')
+      this.props.history.push('/users');
+      window.location.reload();
+    } else {
+      this.state.userCode === ''
+        ? toast.warning('Please enter the Verification Code')
+        : toast.warning('Incorrect verfication code!');
     }
   };
 
   render() {
-
     return (
       <div className="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
         <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
           <div className="flex flex-col overflow-y-auto md:flex-row">
-
             <div className=" flex flex-row h-32 md:h-auto md:w-1/2">
               <img
                 aria-hidden="true"
                 className="mb-14  mr-8 h-70 dark:hidden"
                 src={logo}
                 alt="Office"
-                
               />
               <img
                 aria-hidden="true"
@@ -49,24 +45,28 @@ class VerificationPage extends React.Component {
               />
             </div>
 
-            <div className="h-80 mt-8 w-1 mx-5 shadow-2xl bg-red-200"/>
+            <div className="h-80 mt-8 w-1 mx-5 rounded shadow-2xl bg-gray-200" />
 
             <div className="flex flex-row mt-10  overflow-y-auto md:flex-row">
-              <div className="px-4 py-3 mt-8 w-3/4 bg-white rounded-lg dark:bg-gray-800">
-                <h4 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">Two Factor Authentication</h4>
+              <div className="px-4 py-3 mt-8 w-3/4 bg-white dark:bg-gray-800">
+                <h4 className="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
+                  Two Factor Authentication
+                </h4>
 
-                <form onSubmit={(e) => this.onSubmitHandler(e)}>
+                <form onSubmit={e => this.onSubmitHandler(e)}>
                   <label className="block text-sm">
-                    <span className="text-gray-700 dark:text-gray-400">Please Enter the Two Factor Authentication code sent to your email.</span>
+                    <span className="text-gray-700 dark:text-gray-400">
+                      Please Enter the Two Factor Authentication code sent to your email.
+                    </span>
                   </label>
-                    <input
-                      className="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                      placeholder="Enter your verification code"
-                      id="verificationCode"
-                      onChange={e => this.setState({ userCode: e.target.value })}
-                      autoComplete={'off'}
-                    />
-                  
+                  <input
+                    className="w-full mt-2 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                    placeholder="Enter your verification code"
+                    id="verificationCode"
+                    onChange={e => this.setState({ userCode: e.target.value })}
+                    autoComplete="off"
+                    autoFocus
+                  />
 
                   <button
                     className="flex items-center  mt-5 justify-center w-100 px-auto content-center py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-red-500 border border-transparent shadow-2xl active:bg-red-400 hover:bg-red-400 focus:outline-none focus:shadow-outline-purple w-full"
@@ -75,7 +75,6 @@ class VerificationPage extends React.Component {
                     <span>Submit</span>
                   </button>
                 </form>
-
               </div>
             </div>
           </div>
