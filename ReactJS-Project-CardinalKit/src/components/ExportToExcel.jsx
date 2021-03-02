99cdf66fb1f43fbcd6d9f0a195dffb058b9959bb
varunkumar3091999 <varuns@spritle.com>
2021-03-02 15:46:31 +0530

    export to excel working as expected, styling and code optimisation yet to be done

--- ReactJS-Project-CardinalKit/src/components/ExportToExcel.jsx
+++ ReactJS-Project-CardinalKit/src/components/ExportToExcel.jsx
@@ -1,75 +1,111 @@
-import React, { useState, useEffect } from "react";
-import ReactExport from "react-export-excel";
-import { getQuestions, getSurveys } from "../api/getAllUsers";
+import React, { useState, useEffect } from 'react';
+import ReactExport from 'react-export-excel';
+import { getQuestions, getSurveys } from '../api/getAllUsers';
+import SurveysTable from './SurveysTable';
 
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
 const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
-const ExportToExcel = ({uid}) =>  {
+const ExportToExcel = ({ uid }) => {
+  const [surveyQuestions, setSurveyQuestions] = useState([]);
+  const [response, setResponse] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [excelData, setExcelData] = useState([]);
+  const [dataExtracted, setDataExtracted] = useState(false);
 
-	const [surveyQuestions, setSurveyQuestions] = useState([])
-	const [response, setResponse] = useState([])
-	const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    loading && matchResponseWithQuestions();
+  }, [surveyQuestions]);
 
-	useEffect(() => {
-		loading && matchResponseWithQuestions()
-	}, [surveyQuestions])
+  useEffect(() => {
+    setQuestionAndResponseToState();
+  }, []);
 
-	const exportAsExcel = () => {
-		getSurveys(uid)
-		.then((querySnapshot) => {
-			querySnapshot.docs.map((doc) => {
-				var questionId = doc.data().surveyQuestionId
-				setResponse([...response, {response: doc.data().payload.results, questionId}])
-				getQuestions(questionId)
-					.then((doc) => {	
-						setLoading(true)
-						setSurveyQuestions([...surveyQuestions, {questions: doc.questions, questionId }])
-						setLoading(false)
-					})
-				})
-		})
-	}
+  const setQuestionAndResponseToState = () => {
+    setSurveyQuestions([])
+    setResponse([])
+    setExcelData([])
+    
+    let temprorySurvey = [];
+    let temproryResponse = [];
+    getSurveys(uid).then(querySnapshot => {
+      querySnapshot.docs.map(doc => {
+        var questionId = doc.data().payload.identifier;
+        temproryResponse.push({ response: doc.data().payload.results, questionId });
+        setResponse(temproryResponse);
+        getQuestions(questionId).then(doc => {
+          setLoading(true);
+          temprorySurvey.push({
+            questions: doc.questions,
+            name: doc.name,
+            description: doc.description,
+            questionId,
+          });
+          setSurveyQuestions([...temprorySurvey]);
+          setLoading(false);
+        });
+      });
+    });
+  };
 
-	const fetchResponseForSingleQuestion = (surveyId, questionNumber) => {
-		const responseForCurrentSurvey = response.filter((res) => res.questionId = surveyId)[0]	
-		return responseForCurrentSurvey.response.filter((res) => res.questionNumber === questionNumber)[0].results[0]	
-	}
+  const matchResponseWithQuestions = () => {
+    var tempData = [];
+    var questionId = '';
+    var name = '';
+    var description = '';
+    var answer = '';
 
-	const matchResponseWithQuestions = () => {
-		surveyQuestions.map((survey) => {
-			const surveyId = survey.questionId
-			survey.questions.map(question => {
-				var res = fetchResponseForSingleQuestion(surveyId, question.questionNumber)
-				console.log(res.booleanAnswer?.toString())
-			})
-		})
-	}
+    surveyQuestions.map((survey, i) => {
+      questionId = survey.questionId;
+      name = survey.name;
+      description = survey.description;
+      tempData = survey.questions.map((question, i) => {
+        var matchedResponse = response.find(res => res.questionId === questionId);
+
+        if (matchedResponse.response[i].results[0]?.booleanAnswer !== undefined) {
+          answer = matchedResponse.response[i].results[0]?.booleanAnswer.toString();
+        } else if (matchedResponse.response[i].results[0]?.scaleAnswer) {
+          answer = matchedResponse.response[i].results[0]?.scaleAnswer.toString();
+        } else if (matchedResponse.response[i].results[0]?.choiceAnswers) {
+          answer = question.choices.join(', ');
+        } else {
+          answer = '-';
+        }
+        return Object.assign({}, question, { ...matchedResponse.response[i].results[0], answer });
+      });
+    });
+    setExcelData([...excelData, { data: tempData, questionId, name, description }]);
+    setDataExtracted(true);
+  };
+
+  const renderDownloadButton = () => {
+    if (dataExtracted) {
+      return (
+        <button onClick={() => setQuestionAndResponseToState()}>
+          <span className="px-2 py-1 ml-2 font-semibold bg-blue-200 leading-tight shadow-lg dark:bg-blue-700 dark:text-blue-100">
+            Response
+            <i className="ml-1 fas fa-cloud-download-alt  text-gray-700	animate-bounce ease-out hover:scale-50" />
+          </span>
+        </button>
+      );
+    } else {
+      return null;
+    }
+  };
 
 
-	return (
-		<button onClick={() => {exportAsExcel()}}>
-			<span className="px-2 py-1  font-semibold bg-blue-200 leading-tight rounded-full dark:bg-blue-700 dark:text-blue-100">
-				Response
-				<i className="ml-1 fas fa-cloud-download-alt  text-gray-700	animate-bounce ease-out hover:scale-50" />
-			</span>
-		</button> 
-		// <ExcelFile element={renderDownloadButton()}>
-		// 	<ExcelSheet data={dataSet1} name="Employees">
-		// 		<ExcelColumn label="Name" value="name"/>
-		// 		<ExcelColumn label="Wallet Money" value="amount"/>
-		// 		<ExcelColumn label="Gender" value="sex"/>
-		// 		<ExcelColumn label="Marital Status"
-		// 			value={(col) => col.is_married ? "Married" : "Single"}/>
-		// 	</ExcelSheet>
-		// 	<ExcelSheet data={dataSet2} name="Leaves">
-		// 		<ExcelColumn label="Name" value="name"/>
-		// 		<ExcelColumn label="Total Leaves" value="total"/>
-		// 		<ExcelColumn label="Remaining Leaves" value="remaining"/>
-		// 	</ExcelSheet>
-		// </ExcelFile>
-	);
-}
+  return (
+    <ExcelFile element={renderDownloadButton()} filename="Cardinal Kit survey Response">
+      {excelData.map((survey, i) => (
+        <ExcelSheet data={survey.data} name={survey.name}>
+          <ExcelColumn label="Question Number" value="questionNumber" widthPx={160} />
+          <ExcelColumn label="Question" value="text" wrapText={true} />
+          <ExcelColumn label="Answer" value={'answer'} />
+        </ExcelSheet>
+      ))}
+    </ExcelFile>
+  );
+};
 
-export default ExportToExcel
+export default ExportToExcel;
