066331abbb11ebac78703c07354f9ff0a86b7e82
varunkumar3091999 <varuns@spritle.com>
2021-03-03 17:35:01 +0530

    font size change

--- ReactJS-Project-CardinalKit/src/components/SideBar.jsx
+++ ReactJS-Project-CardinalKit/src/components/SideBar.jsx
@@ -33,7 +33,7 @@ const SideBar = () => (
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={ 2 } d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
               </svg>
               <span><i class="far fa-tachometer-slow"></i></span>
-              <span className="ml-4">Dashboard</span>
+              <span className="ml-4 text-base">Dashboard</span>
             </Link>
           </li>
         </ul>
@@ -50,7 +50,7 @@ const SideBar = () => (
             <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
             </svg>
-              <span className="ml-4">Manage Users</span>
+              <span className="ml-4 text-base">Manage Users</span>
             </Link>
           </li>
         </ul>
@@ -68,7 +68,7 @@ const SideBar = () => (
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
               </svg>
-              <span className="ml-4">Settings</span>
+              <span className="ml-4  text-base">Settings</span>
             </Link>
           </li>
         </ul>
@@ -86,7 +86,7 @@ const SideBar = () => (
             <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-              <span className="ml-4">Create Surveys</span>
+              <span className="ml-4  text-base">Create Surveys</span>
             </Link>
           </li>
         </ul>
