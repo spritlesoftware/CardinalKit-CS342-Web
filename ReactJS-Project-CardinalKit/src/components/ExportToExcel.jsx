ee4dcdebe3e3f844f515ed2f467d4a70ece5715c
varunkumar3091999 <varunkumar0930@gmail.com>
2021-03-17 14:51:29 +0530

    styled button for response download

--- ReactJS-Project-CardinalKit/src/components/ExportToExcel.jsx
+++ ReactJS-Project-CardinalKit/src/components/ExportToExcel.jsx
@@ -102,7 +102,14 @@ const ExportToExcel = ({ uid }) => {
         table={uid}
         filename="Cardinal Kit Response"
         sheet="tablexls"
-        buttonText="Response"
+        buttonText={
+          <div className="flex">
+            Response 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-5 w-5 animate-bounce">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+            </svg>
+          </div>
+        }
       />
       <table id={uid} className="hidden">
         <tbody>
